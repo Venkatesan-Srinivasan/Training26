47c457aa252d609b9b6648f49f611b2e0126c9f4
Venkatesan-Srinivasan <Venkatesan.Srinivasan@trumpf.com>
2026-07-16 10:48:29 +0530

    T1-balanced brackets checker solution updated

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -6,5 +6,36 @@ class Program {
     static void Main (string[] args) {
 
         Console.WriteLine("Hello World!");
+        //Calling the method to check if the brackets are balanced or not
+        bool result = T1_Balanced_Brackets ("([])");
+        //bool result = T1_Balanced_Brackets ("(]");
+
+        Console.WriteLine (result);
+    }
+
+    public static bool T1_Balanced_Brackets (string st) {
+
+        Stack<char> stack = new Stack<char> ();
+        for (int i = 0; i < st.Length; i++) {
+            char c = st[i];
+            if (c == '[') {
+                stack.Push (']');
+                //Console.WriteLine (stack.Peek ());
+            } else if (c == '(') {
+                stack.Push (')');
+                // Console.WriteLine (stack.Peek ());
+            } else {
+                //only the top elemnt matches with the current character then pop the top element from the stack
+
+                if (stack.Peek () == c) {
+                    stack.Pop ();
+                }
+            }
+
+            if (stack.Count () == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
