4f0904f6661f5253c5f4193eb50d6ba82a1eb4ff
Venkatesan-Srinivasan <Venkatesan.Srinivasan@trumpf.com>
2026-07-16 12:30:43 +0530

    T3 -wait for a cooler day solution updated

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,10 +1,46 @@
 ﻿using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Training26;
 
 class Program {
     static void Main (string[] args) {
+        Console.WriteLine ("Hello World!");
+        int[] TestCase = new int[] { 30, 31, 29, 32, 28, 27, 30, 25 };
+        int[] result = T3_CoolerDay (TestCase);
+        Console.Write ('[');
+        foreach (int res in result) {
+            Console.Write (res);
+            Console.Write (' ');
+        }
+        Console.Write (']');
 
-        Console.WriteLine("Hello World!");
     }
+
+    public static int[] T3_CoolerDay (int[] input) {
+        int n = input.Length;
+        int[] result = new int[n];
+        int[] check = new int[n];
+        for (int i = 0, j = 0; i < n - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                check[j] = input[i + 1];
+                j++;
+            }
+        }
+        for(int i=0,j=0,k=0;i<n;i++) {
+            if (check[j] == input[i]) j++;
+            //Console.WriteLine(input.IndexOf (check[j]));
+            if ((input.IndexOf (check[j]) - i) > 0) {
+                result[k] = input.IndexOf (check[j]) - i;
+                Console.WriteLine (result[k]);
+                k++;
+            }
+        }
+        foreach (int j in result) {
+            Console.WriteLine (j);
+        }
+        return result;
+
+    }
+
 }
