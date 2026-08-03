53942129d207c8751e39aba0c064ae6b1884597c
Venkatesan-Srinivasan <Venkatesan.Srinivasan@trumpf.com>
2026-08-03 14:20:23 +0530

    A02: Made review changes

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,41 +1,43 @@
-﻿namespace A02;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        //LET USER THINK OF A RANDOM NUMBER
-        //THE MACHINE SHOULD GUESS WHAT IS THE NUMBER
-
-        int min = 0;
-        int max = 127;
-        Console.WriteLine ("Answer [Y]es or [N]o for the following");
-        string result = "";
-        SecretCheck (min, max);
-
-        void SecretCheck (int l_min, int l_max) {
-            for (int i = 7; i > 0; i--) {
-                int l_mid = (l_min + l_max + 1) / 2;
-                Console.Write ($"Is the number less than {l_mid}: ");
-                var option = Console.ReadKey (true).Key;
-                if (option == ConsoleKey.Y) {
-                    Console.WriteLine ("Yes");
-                    l_max = l_mid;
-                    result += "0";
-                    //if (i == 1)
-                    //   Console.WriteLine ($"{l_mid - 1} is your guess number");
-                }
-                if (option == ConsoleKey.N) {
-                    Console.WriteLine ("No");
-                    l_min = l_mid;
-                    result += "1";
-                    //if (i == 1)
-                    //   Console.WriteLine ($"{l_mid} is your guess Number");
-                }
-                if (option != ConsoleKey.Y && option != ConsoleKey.N)
-                    i++;
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------
+// Program.cs
+// Program to guess the Secret Number in 7 attempts guessed by the user.
+// --------------------------------------------------------------------------------------------
+namespace A02;
+#region class Program -----------------------------------------------------------------------------
+class Program {
+   static void Main (string[] args) {
+      // Let user think a random number
+      int min = 0;
+      int max = 127;
+      Console.WriteLine ("Guess a number from 1 to 100 \n\nAnswer [Y]es or [N]o for the following");
+      SecretCheck (min, max);
+      //Method to guess the secret number at 7 attempts
+      #region Method ------------------------------------------------
+      void SecretCheck (int l_min, int l_max) {
+         for (int i = 7; i > 0; i--) {
+            int l_mid = (l_min + l_max + 1) / 2;
+            Console.Write ($"\tIs the number less than {l_mid}: ");
+            var option = Console.ReadKey (true).Key;
+            if (option == ConsoleKey.Y) {
+               Console.WriteLine ("Yes");
+               l_max = l_mid;
+               if (i == 1)
+                  Console.WriteLine ($"\n\t{l_mid - 1} is your guess number");
             }
-        }
-        Console.WriteLine ($"the answer: {Convert.ToInt32 (result, 2)}");
-    }
+            if (option == ConsoleKey.N) {
+               Console.WriteLine ("No");
+               l_min = l_mid;
+               if (i == 1)
+                  Console.WriteLine ($"\n\t{l_mid} is your guess Number");
+            }
+            if (option != ConsoleKey.Y && option != ConsoleKey.N)
+               i++;
+         }
+      }
+      #endregion
+   }
 }
+#endregion
