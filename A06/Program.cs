6210f90c3b61e972691d21e2d1afc1e0e1235120
Venkatesan-Srinivasan <Venkatesan.Srinivasan@trumpf.com>
2026-08-31 16:20:17 +0530

    Review Fix

--- A06/Program.cs
+++ A06/Program.cs
@@ -4,12 +4,8 @@
 // ------------------------------------------------------------------------------------------------
 // Program.cs
 // A06: 8 Queens problem
-// Program to solve the 8 Queens problem. On a standard chessboard, place 8 queens so that no queen
-// can attack another. Thus, no two queens may share the same row, column, or diagonal.
-// The solution set must contain only distinct solutions,
-// meaning no solution is identical to another by:
-//   - 90°, 180°, or 270° rotation
-//   - mirror image (horizontal or vertical)
+// Program to Solve 8 Queens: place queens so that no two queen share same row, column, or diagonal
+// Keep only distinct solutions (exclude rotations 90°, 180°, 270° and mirror images).
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 using System.Text;
@@ -33,18 +29,13 @@ class Program {
    static void EliminateIdentical (List<int[]> solutions) {
       List<int[]> unique = [];
       foreach (var sln in solutions) {
-         var symmetries = new List<int[]> {
-            ([.. sln.Reverse ()]) // Vertical mirror
-         };
-         // Rotations and its vertical mirror
+         var symmetries = new List<int[]> { ([.. sln.Reverse ()]) }; // Vertical mirror
          int[] rot90 = Rotate90 (sln), vFlipRot90 = [.. rot90.Reverse ()],
                rot180 = Rotate90 (rot90), vFlipRot180 = [.. rot180.Reverse ()],
                rot270 = Rotate90 (rot180), vFlipRot270 = [.. rot270.Reverse ()];
          symmetries.AddRange ([rot90, rot180, rot270, vFlipRot90, vFlipRot180, vFlipRot270]);
-         bool isDuplicate = unique.Any (u => symmetries.Any (a => u.SequenceEqual (a)));
-         if (isDuplicate)
-            continue;
-         unique.Add (sln);
+         if (unique.Any (u => symmetries.Any (a => u.SequenceEqual (a)))) continue;
+         unique.Add (sln); // Adding rotations and its vertical mirror
       }
       solutions.Clear ();
       solutions.AddRange (unique);
@@ -62,14 +53,9 @@ class Program {
    static void PrintChessBoard (int[] sln) {
       OutputEncoding = Encoding.UTF8;
       for (int row = 0, len = 2 * N + 1; row < len; row++) {
-         if (row == 0) // First line
-            WriteLine ($"┌───{GetPattern ("┬───")}┐");
-         // Middle lines which are even lines and not last line
-         else if (row % 2 == 0 && row != 2 * N)
-            WriteLine ($"├───{GetPattern ("┼───")}┤");
-         else if (row == 2 * N) // Last line
-            WriteLine ($"└───{GetPattern ("┴───")}┘");
-         // Queens line- odd lines [1,3,5,7..[/2 makes to access the array [0,1,2,3,..] of solution
+         if (row == 0) WriteLine ($"┌───{GetPattern ("┬───")}┐");
+         else if (row % 2 == 0 && row != 2 * N) WriteLine ($"├───{GetPattern ("┼───")}┤");
+         else if (row == 2 * N) WriteLine ($"└───{GetPattern ("┴───")}┘");
          else {
             for (int col = 0; col < N; col++) {
                string res = (col == sln[row / 2]) ? "│ ♕ " : "│   ";
@@ -87,20 +73,18 @@ class Program {
       }
    }
 
-   //  Solves for identical solutions
+   // Solves for identical solutions
    static void Solve (List<int[]> solutions) {
       int[] sln = new int[N];
       Place (sln, 0, solutions);
 
       // Recursive backtracking method to place queens row by row on the board
       void Place (int[] sln, int r, List<int[]> solutions) {
-         // Choices: one queen per column in each row
-         for (int c = 0; c < N; c++) {
+         for (int c = 0; c < N; c++) { // Choices: one queen per column in each row
             if (IsSafe (sln, r, c)) {
                sln[r] = c;
                Place (sln, r + 1, solutions); // Backtrack: Remove last placed queen
-               // Base case: if all rows are filled (r == n), we found a valid solution
-               if (r + 1 == N) solutions.Add ([.. sln]);
+               if (r + 1 == N) solutions.Add ([.. sln]); // Base case: r == n --> valid solution
             }
          }
       }
@@ -109,8 +93,7 @@ class Program {
       bool IsSafe (int[] sln, int row, int col) {
          for (int i = 0; i < row; i++) {
             int c = sln[i];
-            if (c == col || Math.Abs (c - col) == Math.Abs (i - row))
-               return false;
+            if (c == col || Math.Abs (c - col) == Math.Abs (i - row)) return false;
          }
          return true;
       }
